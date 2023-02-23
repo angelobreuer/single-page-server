4cad03ac243829c3dcac581da2a7e5e4ca0b3921
Angelo Breuer <angelobr@outlook.de>
2023-02-23 09:09:16 +0100

    Initial Commit

---
+++ SinglePageServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRouting();
+builder.Services.AddResponseCaching();
+builder.Services.AddResponseCompression();
+
+var app = builder.Build();
+
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings.Remove(".m3u8");
+provider.Mappings[".m3u8"] = "audio/x-mpegurl";
+
+app.UseStatusCodePages();
+app.UseResponseCaching();
+app.UseResponseCompression();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = provider,
+    ServeUnknownFileTypes = true,
+    HttpsCompression = HttpsCompressionMode.Compress,
+
+});
+
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
